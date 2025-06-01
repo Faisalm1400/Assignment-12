d3c563a244c2bd4681db79b28eefe6da42012f6c
Faisal <faisalm1400@gmail.com>
2025-06-02 01:18:51 +0600

    added delete function

--- src/pages/Articles/MyArticles.jsx
+++ src/pages/Articles/MyArticles.jsx
@@ -2,10 +2,32 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyArticles = () => {
     const { user } = useAuth()
     const [articles, setArticles] = useState([]);
+
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to recover this article!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(`http://localhost:5000/articles/${id}`)
+                    .then(() => {
+                        Swal.fire("Deleted!", "Your article has been deleted.", "success");
+                        setArticles(articles.filter(article => article._id !== id));
+                    })
+            }
+        });
+    };
 
     useEffect(() => {
         if (user?.email) {
@@ -46,7 +68,7 @@ const MyArticles = () => {
                                     <button className="bg-blue-500 text-white px-2 py-1 rounded">Details</button>
                                 </Link>
                                 <button className="bg-yellow-500 text-white px-2 py-1 rounded">Update</button>
-                                <button className="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
+                                <button className="bg-red-500 text-white px-2 py-1 rounded" onClick={() => handleDelete(article._id)}>Delete</button>
                             </td>
                         </tr>
                     ))}
